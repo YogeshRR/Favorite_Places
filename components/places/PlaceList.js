aa965ba4a44451c90a533d1eb6520b492cee3610
Yogesh Raut <yogesh@Yogeshs-MacBook-Air.local>
2024-03-12 05:04:11 +0530

    191 Editing Favorite Place Items

--- components/places/PlaceList.js
+++ components/places/PlaceList.js
@@ -1,5 +1,30 @@
-import { FlatList } from "react-native";
+import { FlatList, View, Text, StyleSheet } from "react-native";
 
 function PlaceList({ places }) {
-  return <FlatList data={places} keyExtractor={(item) => item.id} />;
+  if (!places || places.length === 0) {
+    return (
+      <View style={styles.fallBackContainer}>
+        <Text style={styles.fallBackText}>You don't have Places now</Text>
+      </View>
+    );
+  }
+  return (
+    <FlatList
+      data={places}
+      keyExtractor={(item) => item.id}
+      renderItem={({ item }) => <PlaceItem place={item} />}
+    />
+  );
 }
+
+const styles = StyleSheet.create({
+  fallBackContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignContent: "center",
+  },
+
+  fallBackText: {
+    fontSize: 16,
+  },
+});
